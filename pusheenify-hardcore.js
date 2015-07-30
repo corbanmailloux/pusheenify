0b97bdc659cfc6df87357fae4941c1d41f3ef615
corbanmailloux <cdm3806@rit.edu>
2015-07-30 19:39:41 -0400

    Hardcode pusheenification.

---
+++ pusheenify-hardcore.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByTagName("*");
+for (var i = 0; i < elements.length; i++)
+{
+	elements[i].style.background = "url(https://raw.githubusercontent.com/corbanmailloux/pusheenify/master/pusheen-background.png) repeat fixed";
+}
